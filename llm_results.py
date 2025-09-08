104ab1939525571410c9add38cc197e4201611cd
rverma345 <vermaron9@gmail.com>
2025-09-08 14:08:30 +0530

    new change

--- llm_results.py
+++ llm_results.py
@@ -7,8 +7,8 @@ load_dotenv()
 parser = StrOutputParser()
 
 llm = ChatOpenAI(
-    model='gpt-5-nano-2025-08-07'
-    
+    model='gpt-5-mini-2025-08-07',
+    temperature=0.3
 )
 
 def generate_result(query, context):
